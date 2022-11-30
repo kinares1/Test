dfd439dbd3a50ca519f38358e2ae4c87b1305d7d
Denis <116779712+kinares1@users.noreply.github.com>
2022-11-30 21:27:51 +0700

    Add files via upload

---
+++ 6/6.2.py
@@ -0,0 +1,7 @@
+numbers = [10.2, -2.2, 0, 1.1, 0.5]
+if len(numbers) == 0:
+    print(0)
+else:
+    result = max(numbers)-min(numbers)
+    print (round(result,3))
+#Решенная
---
+++ 6/6.3.py
@@ -0,0 +1,3 @@
+elements = (-20, -5, 10, 15)
+print (sorted(elements, key = abs))
+#Решенная
